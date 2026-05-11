048d2a41507e8dcd73fcdd0a6cf304189dba1650
GabrielPKTN <gabriellacerda20059@outlook.com>
2026-05-11 20:53:29 -0300

    feat: Adiciona triggers de pacientes

--- banco/scripts/procedures/prc_paciente.sql
+++ banco/scripts/procedures/prc_paciente.sql
@@ -1,3 +1,43 @@
+-- --------- Legenda ------------
+-- p = parametro
+-- v = variavel
+-- ------------------------------
+
+
+DELIMITER $$
+
+CREATE TRIGGER trg_habilidade_paciente
+AFTER INSERT ON tb_paciente
+FOR EACH ROW
+BEGIN
+
+	INSERT INTO tb_paciente_habilidade(id_paciente, id_habilidade, idade_meses) VALUES
+    (NEW.id, 1, 0.0),
+    (NEW.id, 2, 0.0),
+    (NEW.id, 3, 0.0),
+    (NEW.id, 4, 0.0),
+    (NEW.id, 5, 0.0);
+
+END$$
+
+DELIMITER ;
+
+DELIMITER $$
+
+CREATE TRIGGER trg_habilidade_paciente
+AFTER INSERT ON tb_paciente
+FOR EACH ROW
+BEGIN
+    
+	INSERT INTO tb_formulario(id_paciente, id_atividade_portage, id_resposta)
+    SELECT NEW.id, id, NULL
+    FROM tb_atividade_portage
+    ORDER BY id ASC;
+
+END$$
+
+DELIMITER ;
+
 DELIMITER $$
 
 CREATE PROCEDURE proc_cria_habilidades_paciente(
