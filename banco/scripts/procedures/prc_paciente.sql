3e94f60302ed0f1dbdbe309b49ccad4ee35344aa
GabrielPKTN <gabriellacerda20059@outlook.com>
2026-05-13 20:54:42 -0300

    feat: Adiciona procedure que retorna o paciente pelo cpf

--- banco/scripts/procedures/prc_paciente.sql
+++ banco/scripts/procedures/prc_paciente.sql
@@ -112,7 +112,7 @@ CREATE PROCEDURE prc_buscar_paciente_completo(
                 'id', p_id_paciente,
                 'foto', v_foto,
                 'nome', v_nome,
-                'data_nascimento', v_data_nascimento,
+                'data_nascimento', DATE_FORMAT(v_data_nascimento, '%d/%m/%Y'),
                 'idade', TIMESTAMPDIFF(YEAR, v_data_nascimento, CURDATE()),
                 'cpf', v_cpf,
                 'grau_suporte', v_grau_suporte,
@@ -407,9 +407,6 @@ END $$
 
 DELIMITER ;
 
--- CALL proc_delete_paciente(2, 1, @resultDeletePaciente);
-select @resultDeletePaciente;
-
 DELIMITER $$
 
 CREATE PROCEDURE prc_inserir_relacao_usuario_paciente(
@@ -482,8 +479,68 @@ DELIMITER ;
 DELIMITER $$
 
 CREATE PROCEDURE prc_retorna_paciente_pelo_cpf(
+
+	IN p_cpf VARCHAR(20),
+    OUT p_message JSON
+
 ) BEGIN
+	
+	DECLARE v_id INT;
+    DECLARE v_nome VARCHAR(150);
+    DECLARE v_foto VARCHAR(255);
+    DECLARE v_data_nascimento DATE;
+    DECLARE v_serie VARCHAR(30);
+    DECLARE v_grau VARCHAR(30);
+	DECLARE data_hoje DATE;
+    SET data_hoje = curdate();
+
+	IF NOT EXISTS(
+		SELECT 1
+        FROM tb_paciente
+        WHERE cpf = p_cpf
+    ) THEN  SET p_message = JSON_OBJECT(
+            'status', FALSE,
+            'status_code', 404,
+            'message', 'Paciente não encontrado',
+            'date', DATE_FORMAT(data_hoje, '%d/%m/%Y')
+        );
+    
+    ELSE
+    
+		SELECT id_paciente, nome, foto, data_nascimento, serie, grau 
+        FROM vw_data_paciente WHERE cpf = p_cpf
+        INTO v_id, v_nome, v_foto, v_data_nascimento, v_serie, v_grau;
+        
+        
+        SET p_message = JSON_OBJECT(
+			'status', TRUE,
+            'status_code', 200,
+            'message', 'Requisição bem sucedida!!!',
+            'date', DATE_FORMAT(data_hoje, '%d/%m/%Y'),
+            'data', JSON_OBJECT(
+            
+				'id', v_id,
+                'nome', v_nome,
+                'foto', v_foto,
+                'cpf', p_cpf,
+                'data_nascimento', DATE_FORMAT(v_data_nascimento, '%d/%m/%Y'),
+                'idade', TIMESTAMPDIFF(YEAR, v_data_nascimento, CURDATE()),
+                'serie_escolar', v_serie,
+                'grau_suporte', v_grau
+            
+            )
+        );
+    
+    END IF;
 
 END$$
 
 DELIMITER ;
+
+
+-- @returnPacienteCpf
+-- @resultUsuario
+-- @resultDeletePaciente
+-- @resultInsertRelation
+-- @resultUpdatePaciente
+-- @resultInsertPaciente
