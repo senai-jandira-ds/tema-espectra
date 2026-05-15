65441d0603a05d1577244cd011ef1d3b0c13cc10
GABRIEL LACERDA CORREIA <25132902@dsn127m1242316.cfp127aluno.local>
2026-05-15 08:14:10 -0300

    fix: Atualiza valor da coluna idade_meses da tabela paciente_habilidade

--- banco/scripts/espectra_script.sql
+++ banco/scripts/espectra_script.sql
@@ -115,7 +115,7 @@ CREATE TABLE tb_habilidade(
 CREATE TABLE tb_paciente_habilidade(
 		
 	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-	idade_meses DECIMAL(10,1) NOT NULL,
+	idade_meses DECIMAL(10,4) NOT NULL,
 	id_paciente INT NOT NULL,
 	id_habilidade INT NOT NULL,
 	
@@ -244,12 +244,6 @@ CREATE TABLE tb_formulario(
 		FOREIGN KEY (id_resposta)  REFERENCES tb_resposta_formulario(id)
     
     );
-
--- ----------------------------------
--- INSERTS
--- ----------------------------------
-USE db_espectra;
-
 
 INSERT INTO tb_tipo_usuario(tipo_usuario) VALUES ('Psicopedagogo'),('Responsável');
 
@@ -1002,84 +996,7 @@ ORDER BY id ASC;
 INSERT INTO tb_formulario (id_paciente, id_atividade_portage, id_resposta)
 SELECT 5, id, NULL 
 FROM tb_atividade_portage 
-ORDER BY id ASC;
-
-DELIMITER $$
-
-CREATE TRIGGER trg_deleta_tentativas_atividade
-BEFORE DELETE ON tb_atividade
-FOR EACH ROW
-BEGIN
-    
-    DELETE FROM tb_tentativa WHERE id_atividade = OLD.id;
-
-END$$
-
-DELIMITER ;
-
-DELIMITER $$
-
-CREATE TRIGGER trg_deleta_atividade_personalizada_atividade
-AFTER DELETE ON tb_atividade
-FOR EACH ROW
-BEGIN
-    
-    IF (OLD.id_atividade_personalizada IS NOT NULL) THEN
-    
-    DELETE FROM tb_atividade_personalizada WHERE id = OLD.id_atividade_personalizada;
-    
-    END IF;
-
-END$$
-
-DELIMITER ;
-
-DELIMITER $$
-
-CREATE TRIGGER trg_habilidade_paciente
-AFTER INSERT ON tb_paciente
-FOR EACH ROW
-BEGIN
-
-	INSERT INTO tb_paciente_habilidade(id_paciente, id_habilidade, idade_meses) VALUES
-    (NEW.id, 1, 0.0),
-    (NEW.id, 2, 0.0),
-    (NEW.id, 3, 0.0),
-    (NEW.id, 4, 0.0),
-    (NEW.id, 5, 0.0);
-
-END$$
-
-DELIMITER ;
-
-DELIMITER $$
-
-CREATE TRIGGER trg_formulario_paciente
-AFTER INSERT ON tb_paciente
-FOR EACH ROW
-BEGIN
-    
-	INSERT INTO tb_formulario(id_paciente, id_atividade_portage, id_resposta)
-    SELECT NEW.id, id, NULL
-    FROM tb_atividade_portage
-    ORDER BY id ASC;
-
-END$$
-
-DELIMITER ;
-
-DELIMITER $$
-
-CREATE TRIGGER trg_delete_diagnostico_paciente
-BEFORE UPDATE ON tb_paciente
-FOR EACH ROW
-BEGIN
-
-	DELETE FROM tb_paciente_transtorno WHERE id_paciente = OLD.id;
-
-END $$
-
-DELIMITER ;
+ORDER BY id ASC;    
 
 -- Dados do paciente (serie e grau de suporte ) pelo id
 CREATE VIEW vw_data_paciente AS
@@ -1156,6 +1073,20 @@ FROM tb_usuario usuario
     paciente.id = relacao.id_paciente
     WHERE usuario.id_tipo_usuario = 2
     ORDER BY paciente.id ASC;
+
+CREATE VIEW vw_formulario_usuario AS
+SELECT
+	formulario.id_paciente as id_paciente,
+	atividade.id as id_atividade,
+    atividade.numero_questao as numero_questao,
+    atividade.comportamento as comportamento,
+    atividade.id_faixa_idade as id_faixa_idade,
+    atividade.id_habilidade as id_habilidade,
+	formulario.id_resposta as id_resposta
+FROM tb_formulario formulario
+	JOIN tb_atividade_portage atividade ON
+    atividade.id = formulario.id_atividade_portage
+    ORDER BY id_atividade_portage ASC;
 
 DELIMITER $$
 
@@ -1628,6 +1559,8 @@ CREATE PROCEDURE prc_deleta_usuario(
     END IF;
 
 END$$
+    
+DELIMITER ;
 
 DELIMITER $$
 
@@ -2200,3 +2133,293 @@ END$$
 
 DELIMITER ;
 
+DELIMITER $$
+
+CREATE PROCEDURE prc_atualizar_respostas_formulario(
+	IN p_id_usuario INT,
+    IN p_id_paciente INT,
+    IN p_lista_respostas JSON,
+    OUT p_message JSON
+) BEGIN 
+
+	DECLARE data_hoje DATE;
+    SET data_hoje = CURDATE();
+
+	IF NOT EXISTS(SELECT 1 FROM tb_paciente WHERE id = p_id) THEN
+    
+		SET p_message = JSON_OBJECT(
+        
+			'status', FALSE,
+			'status_code', 404,
+			'message', 'Não foram encontrados dados de retorno!!!',
+			'date', DATE_FORMAT(data_hoje, '%d/%m/%Y')
+        
+        );
+    
+    ELSEIF NOT EXISTS(SELECT 1 FROM tb_usuario_paciente WHERE id_usuario = p_id_usuario AND id_paciente = p_id_paciente) THEN
+    
+		SET p_message = JSON_OBJECT(
+        
+			'status', FALSE,
+			'status_code', 401,
+			'message', 'Não foi possível processar a requisição pois faltam credenciais válidas!!!',
+			'date', DATE_FORMAT(data_hoje, '%d/%m/%Y')
+			
+		);
+    
+    ELSEIF NOT EXISTS (SELECT 1 FROM tb_usuario WHERE id = p_id_usuario AND id_tipo_usuario = 1) THEN
+    
+		SET p_message = JSON_OBJECT(
+        
+			'status', FALSE,
+			'status_code', 401,
+			'message', 'Não foi possível processar a requisição pois faltam credenciais válidas!!!',
+			'date', DATE_FORMAT(data_hoje, '%d/%m/%Y')
+			
+		);
+    
+	ELSE
+    
+		-- INICIA UPDATE NA TABELA FORMULARIO
+		UPDATE tb_formulario
+		JOIN JSON_TABLE( -- JOIN CRIA A TABELA VIRTUAL, E JSON_TABLE É RESPONSÁVEL POR LER OS JSONS
+			p_lista_respostas, -- ARRAY JSON VINDO DO BACK
+			'[*]' COLUMNS ( -- [*] QUER DIZER: PERCORRA TODOS OS OBJETOS DENTRO DO ARRAY JSON
+				id_atividade_portage INT PATH '$.id_atividade_portage', -- CRIA COLUNA VIRTUAL, O PATH INDICA ONDE PROCURAR NO JSON
+				id_resposta INT PATH '$.id_resposta'
+			)
+		) AS novo_formulario 
+		
+		ON tb_formulario.id_atividade_portage = novo_formulario.id_atividade_portage -- ENCONTRE A LINHA DE TB_FORMULARIO ONDE O ID DA ATIVIDADE PORTAGE SEJA IGUAL A QUE VEM DO JSON
+		SET tb_formulario.id_resposta = novo_formulario.id_resposta -- PEGUE O VALOR DE RESPOSTA QUE ESTÁ NO BANCO E SUBSTITUA PELO VALOR DE RESPOSTA VINDO DO JSON
+		WHERE tb_formulario.id_paciente = p_id_paciente;
+		
+		SET p_message = JSON_OBJECT(
+            'status', TRUE,
+            'status_code', 200,
+            'message', 'Item atualizado com sucesso!!!',
+            'date', DATE_FORMAT(data_hoje, '%d/%m/%Y')
+        );
+        
+        CALL prc_buscar_paciente_completo(p_id_paciente, @resultPaciente);
+        
+    END IF;
+
+END $$
+
+DELIMITER ;
+
+DELIMITER $$
+
+CREATE PROCEDURE prc_formulario_pelo_id_paciente(
+	IN p_id_usuario INT,
+    IN p_id_paciente INT,
+    OUT p_message JSON
+	
+) BEGIN
+
+	DECLARE array_comportamento JSON;
+	DECLARE data_hoje DATE;
+    SET data_hoje = CURDATE();
+    
+    
+    IF NOT EXISTS (SELECT 1 FROM tb_paciente WHERE id = p_id_paciente) THEN
+    
+		SET p_message = JSON_OBJECT(
+        
+			'status', FALSE,
+			'status_code', 404,
+			'message', 'Não foram encontrados dados de retorno!!!',
+			'date', DATE_FORMAT(data_hoje, '%d/%m/%Y')
+        
+        );
+    
+    ELSEIF NOT EXISTS (SELECT 1 FROM tb_usuario WHERE id = p_id_usuario) THEN
+    
+		SET p_message = JSON_OBJECT(
+        
+			'status', FALSE,
+			'status_code', 404,
+			'message', 'Não foram encontrados dados de retorno!!!',
+			'date', DATE_FORMAT(data_hoje, '%d/%m/%Y')
+        
+        );
+        
+	ELSEIF NOT EXISTS (SELECT 1 FROM tb_usuario WHERE id = p_id_usuario AND id_tipo_usuario = 1) THEN
+    
+		SET p_message = JSON_OBJECT(
+        
+			'status', FALSE,
+			'status_code', 401,
+			'message', 'Não foi possível processar a requisição pois faltam credenciais válidas!!!',
+			'date', DATE_FORMAT(data_hoje, '%d/%m/%Y')
+			
+		);
+        
+	ELSEIF NOT EXISTS (SELECT 1 FROM tb_usuario_paciente WHERE id_paciente = p_id_paciente AND id_usuario = p_id_usuario) THEN
+    
+		SET p_message = JSON_OBJECT(
+        
+			'status', FALSE,
+			'status_code', 401,
+			'message', 'Não foi possível processar a requisição pois faltam credenciais válidas!!!',
+			'date', DATE_FORMAT(data_hoje, '%d/%m/%Y')
+			
+		);
+        
+	ELSE
+		
+        SELECT JSON_ARRAYAGG(
+            JSON_OBJECT(
+				'id_atividade', id_atividade,
+                'numero_questao', numero_questao,
+                'comportamento', comportamento,
+                'id_faixa_idade', id_faixa_idade,
+                'id_habilidade', id_habilidade,
+                'id_resposta', id_resposta
+            )
+        ) FROM vw_formulario_usuario WHERE id_paciente = p_id_paciente
+        INTO array_comportamento;
+        
+		SET p_message = JSON_OBJECT(
+            'status', TRUE,
+            'status_code', '200',
+            'message', 'Formulario encontrado',
+            'date', DATE_FORMAT(data_hoje, '%d/%m/%Y'),
+            'data', array_comportamento
+        );
+    
+    END IF;
+
+END$$
+
+DELIMITER ;
+
+DELIMITER $$
+
+CREATE TRIGGER trg_update_idade_formulario
+AFTER UPDATE ON tb_formulario
+FOR EACH ROW
+BEGIN
+	
+    -- SE ATUALIZADO PARA SIM
+	IF (NEW.id_resposta = 1) THEN
+		
+        -- VERIFICA SE TEM UMA ATIVIDADE COM ESSE ID DE ATIVIDADE PORTAGE
+		IF EXISTS (
+			SELECT 1 FROM tb_atividade WHERE id_atividade_portage = OLD.id_atividade_portage
+        ) THEN
+        
+			-- SE TIVER, MARCA COMO CONCLUÍDA
+			UPDATE tb_atividade SET
+				concluida = TRUE
+                WHERE id_atividade_portage = OLD.id_atividade_portage;
+		
+        ELSE
+			
+            -- SE NÃO INSERE UMA CONCLUÍDA
+			INSERT INTO tb_atividade(
+				concluida,
+                id_paciente,
+                id_atividade_portage
+            ) VALUES (
+				TRUE,
+                OLD.id_paciente,
+				OLD.id_atividade_portage
+            );
+        
+	END IF;
+		
+        -- ATUALIZA O VALOR DA IDADE EM MESES NA TABELA HABILIDADE_PACIENTE (GRÁFICO)
+		UPDATE tb_habilidade_paciente
+        SET idade_meses = idade_meses + (
+			SELECT
+				valor_atividade
+            FROM tb_faixa_idade
+            JOIN tb_atividade_portage ON
+            tb_atividade_portage.id_faixa_idade = tb_faixa_idade.id
+            WHERE tb_atividade_portage.id = OLD.id_atividade_portage
+        ) WHERE id_paciente = OLD.id_paciente
+			AND id_habilidade = (SELECT id_habilidade FROM tb_atividade_portage WHERE id = OLD.id_atividade_portage);
+        
+	-- CASO A RESPOSTA TENHA SIDO "NÃO" OU "SIM, COM MEDIAÇÃO"
+	ELSEIF (NEW.id_resposta = 2 OR NEW.id_resposta = 3) THEN
+    
+		-- ELE VERIFICA SE ANTERIORMENTE A RESPOSTA ESTAVA MARCADA COMO SIM E SUBTRAI O VALOR DESSA ATIVIDADE PORTAGE
+		IF(OLD.id_resposta = 1) THEN
+        
+			UPDATE tb_habilidade_paciente
+			SET idade_meses = idade_meses - (
+				SELECT
+					valor_atividade
+				FROM tb_faixa_idade
+				JOIN tb_atividade_portage ON
+				tb_atividade_portage.id_faixa_idade = tb_faixa_idade.id
+				WHERE tb_atividade_portage.id = OLD.id_atividade_portage
+			) WHERE id_paciente = OLD.id_paciente
+				AND id_habilidade = (SELECT id_habilidade FROM tb_atividade_portage WHERE id = OLD.id_atividade_portage);
+                
+			-- VERIFICA SE TEM UMA ATIVIDADE COM ESSE ID DE ATIVIDADE PORTAGE MARCADA COMO CONCLUIDA NO BANCO
+			IF EXISTS(SELECT 1 FROM tb_atividade WHERE id_atividade_portage = OLD.id_atividade_portage AND CONCLUIDA = 1) THEN
+				
+				-- CASO TENHA, ELE DEFINE ELA COMO NÃO CONCLUIDA PARA DESENVOLVER
+				UPDATE tb_atividade SET
+					concluida = FALSE
+				WHERE id_atividade_portage = OLD.id_atividade_portage;
+				
+			END IF;
+        
+        END IF;
+    
+    END IF;
+
+END$$
+
+DELIMITER ;
+
+DELIMITER $$
+
+CREATE TRIGGER trg_habilidade_paciente
+AFTER INSERT ON tb_paciente
+FOR EACH ROW
+BEGIN
+
+	INSERT INTO tb_paciente_habilidade(id_paciente, id_habilidade, idade_meses) VALUES
+    (NEW.id, 1, 0.0),
+    (NEW.id, 2, 0.0),
+    (NEW.id, 3, 0.0),
+    (NEW.id, 4, 0.0),
+    (NEW.id, 5, 0.0);
+
+END$$
+
+DELIMITER ;
+
+DELIMITER $$
+
+CREATE TRIGGER trg_formulario_paciente
+AFTER INSERT ON tb_paciente
+FOR EACH ROW
+BEGIN
+    
+	INSERT INTO tb_formulario(id_paciente, id_atividade_portage, id_resposta)
+    SELECT NEW.id, id, NULL
+    FROM tb_atividade_portage
+    ORDER BY id ASC;
+
+END$$
+
+DELIMITER ;
+
+DELIMITER $$
+
+CREATE TRIGGER trg_delete_diagnostico_paciente
+BEFORE UPDATE ON tb_paciente
+FOR EACH ROW
+BEGIN
+
+	DELETE FROM tb_paciente_transtorno WHERE id_paciente = OLD.id;
+
+END $$
+
+DELIMITER ;
